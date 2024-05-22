b26939c82c398e9b2d2d0c2aaa84227040bd3b45
Praveen Bhosle <praveen.sarvagram7075@gmail.com>
2024-05-22 08:06:09 +0530

    My first commit

---
+++ src/com/velocity/Example/Demo.java
@@ -0,0 +1,23 @@
+package com.velocity.Example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Demo {
+
+	public static void main(String[] args) {
+	
+		Demo demo = new Demo();
+		demo.m1();
+	}
+	
+	public void m1() {
+		List<String> list = new ArrayList<String>();
+		list.add("Core-Java");
+		list.add("Collection Framework");
+		list.add("My-SQL");
+		list.add("Git");
+		System.out.println(list);
+	}
+
+}

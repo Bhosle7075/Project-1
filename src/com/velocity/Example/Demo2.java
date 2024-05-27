96788155f2d2442c91e2fcfe84329116a60b9e14
Bhosle7075 <150243711+Bhosle7075@users.noreply.github.com>
2024-05-27 08:49:52 +0530

    Update Demo2.java

--- src/com/velocity/Example/Demo2.java
+++ src/com/velocity/Example/Demo2.java
@@ -5,8 +5,10 @@ public class Demo2 {
 	public static void main(String[] args) {
 		System.out.println("Hello All");
 		System.out.println("Welcome to git");
-		System.out.println("Writing New Message");
 		System.out.println("Welcome to conflict resolving");
+		for(int i= 0; i<=10;i++){
+			System.out.println("Welcome to Velocity");
+		}
 	}
 
 }

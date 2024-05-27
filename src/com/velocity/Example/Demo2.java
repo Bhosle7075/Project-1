8c16648c1b74a94011db78c247165dc79aa67c87
Bhosle7075 <150243711+Bhosle7075@users.noreply.github.com>
2024-05-27 07:52:00 +0530

    Added Conflict messsage

--- src/com/velocity/Example/Demo2.java
+++ src/com/velocity/Example/Demo2.java
@@ -5,6 +5,7 @@ public class Demo2 {
 	public static void main(String[] args) {
 		System.out.println("Hello All");
 		System.out.println("Welcome to git");
+		System.out.println("Welcome to conflict resolving");
 	}
 
 }
